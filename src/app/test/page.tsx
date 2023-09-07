e08cbeae2a59594c309af0230940abfa5209d644
mattwelter <welter.m@icloud.com>
2023-09-07 12:42:54 -0700

    Commit, for fun

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -5,13 +5,6 @@ export default async function Page({ params }: {
     params: { fid: string }
 }) {
 
-
-    // host: 44.200.139.115
-    // port: 5432
-    // database: farcaster
-    // user: b8d150
-    // password: 91A1C66F-B750-4790-B81F-D11822764EAE
-
     const sql = postgres('postgres://b8d150:91A1C66F-B750-4790-B81F-D11822764EAE@44.200.139.115:5432/farcaster')
 
     async function getTestData() {
