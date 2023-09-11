f318e2873324687ed99073515c3685edc0652e9e
mattwelter <welter.m@icloud.com>
2023-09-11 12:40:09 -0700

    Test

--- next.config.js
+++ next.config.js
@@ -6,7 +6,20 @@ const nextConfig = {
         dest: 'public',
         rgister: true,
         skipWaiting: true,
-    })
+    }),
+    async headers() {
+        return [
+          {
+            source: '/',
+            headers: [
+              {
+                key: 'Cache-Control',
+                value: 's-maxage=1, stale-while-revalidate=59',
+              },
+            ],
+          },
+        ];
+      },
 }
 
 module.exports = nextConfig
