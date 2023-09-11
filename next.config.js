c8410d1a1aa3e1f22cd92f906e037999fedcdb8b
mattwelter <welter.m@icloud.com>
2023-09-11 12:46:34 -0700

    Revert back

--- next.config.js
+++ next.config.js
@@ -6,20 +6,7 @@ const nextConfig = {
         dest: 'public',
         rgister: true,
         skipWaiting: true,
-    }),
-    async headers() {
-        return [
-          {
-            source: '/',
-            headers: [
-              {
-                key: 'Cache-Control',
-                value: 's-maxage=1, stale-while-revalidate=59',
-              },
-            ],
-          },
-        ];
-      },
+    })
 }
 
 module.exports = nextConfig
