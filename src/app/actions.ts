0a797fd612c94567a995a04d1508c90340853d1a
mattwelter <welter.m@icloud.com>
2023-09-13 11:20:58 -0700

    Fixed case sensitive search

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -2,7 +2,7 @@
 
 export async function searchUsername(formData: FormData) {
   const _username = formData.get('username') as string
-  const username = _username.replaceAll(' ', '').replaceAll('@', '')
+  const username = _username.toLowerCase().replaceAll(' ', '').replaceAll('@', '')
   const getUser = await fetch(
     `https://api.neynar.com/v1/farcaster/user-by-username/?api_key=${process.env.NEYNAR_API_KEY}&username=${username}`,
     { method: 'GET' }
