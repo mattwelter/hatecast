9995c78424babd1cd958d771bd7e704f48f85d86
mattwelter <welter.m@icloud.com>
2023-09-16 14:57:40 -0700

    Created analytics pages for 1, 7, and 30 days

---
+++ src/app/analytics/page.tsx
@@ -0,0 +1,25 @@
+import Analytics1Day from './../components/Analytics1Day'
+import Analytics7Day from './../components/Analytics7Day'
+import Analytics30Day from './../components/Analytics30Day'
+import LoadingFeed from './../components/LoadingFeed'
+import { Suspense } from 'react'
+
+export default async function Page() {
+
+    return (
+        <main>
+            <a className="backButton" href="/">Back Home</a>
+            <div className="header userFeedHeader">
+                <h1>Analytics</h1>
+            </div>
+            <div>
+                <h2 className="recentlyUnfollowed">Most unfollowed in 7 days</h2>
+                <Suspense fallback={<LoadingFeed />}>
+                    <Analytics30Day />
+                </Suspense>
+                <a className="noMoreCaption">No more unfollows</a>
+                <a className="mostRecentCaption">Only showing 10 most recent</a>
+            </div>
+        </main>
+    )
+}
---
+++ src/app/components/Analytics7Day.tsx
@@ -0,0 +1,44 @@
+import sql from '../db.js'
+
+export default async function Analytics7Day() {
+
+  const getFeed = async function(){
+    const data = await sql`
+        SELECT target_fid, COUNT(target_fid) AS occurrence_count
+        FROM links
+        WHERE deleted_at >= NOW() - INTERVAL '7 days'
+        GROUP BY target_fid
+        ORDER BY occurrence_count DESC
+        LIMIT 10;
+      `
+    return data
+  }
+
+  const unfollows = await getFeed()
+
+  // Get username for each fid
+  if (unfollows.length > 0){
+    for (let i=0; i<unfollows.length; i++){
+      const getUser = await fetch(`https://api.neynar.com/v1/farcaster/user/?api_key=${process.env.NEYNAR_API_KEY}&fid=${unfollows[i].target_fid}`, { method: "GET" });
+      const userResponse = await getUser.json();
+      unfollows[i].username = userResponse.result.user.username;
+    }
+  }
+
+  return (
+    <>
+       {unfollows.length != 0 ? unfollows.map((event: any) => (
+
+          <div className="unfollowCard">
+            <h3>@<a href={"/users/" + event.target_fid}>{ event.username }</a> was unfollowed { event.occurrence_count } times</h3>
+          </div>
+          )) :
+          
+          <div className="unfollowCard">
+            <h3>Looks like no one unfollowed you.</h3>
+          </div>
+          
+        }
+    </>
+    )
+}
