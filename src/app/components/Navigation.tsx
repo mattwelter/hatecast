7d377ff104c7c05822e319a4052836a693c8025d
mattwelter <welter.m@icloud.com>
2023-09-18 09:51:02 -0700

    Highlights tab in nav page is seen

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -1,15 +1,25 @@
-export default async function Analytics7Day() {
+"use client";
 
-  return (
-    <>
-        <div className="navigation">
-            <nav>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/trending">Trending</a></li>
-                </ul>
-            </nav>
-        </div>
-    </>
+import style from '../css/HomePage.module.css'
+import { usePathname } from 'next/navigation'
+
+
+export default function Navigation() {
+
+    const pathname = usePathname()
+    let home = pathname == "/" ? style.selectedTab : ""
+    let trending = pathname == "/trending" ? style.selectedTab : ""
+
+    return (
+        <>
+            <div className="navigation">
+                <nav>
+                    <ul>
+                        <li><a className={home} href="/">Home</a></li>
+                        <li><a className={trending} href="/trending">Trending</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </>
     )
 }
