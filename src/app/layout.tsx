251fc36896534fb96cccfb0e668f53335fa5e89e
mattwelter <welter.m@icloud.com>
2023-09-19 23:31:21 -0700

    Added social metadata on layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
 import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react';
 import './globals.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: `Hatecast`,
+  description: 'Hatecast - Reveal your unfollowers on Farcaster',
+  manifest: '/manifest.json',
+  icons: { apple: '/hatecast_logo.png' },
+  themeColor: '#1B1A1F'
+}
 
 const inter = Inter({
   subsets: ['latin'],
@@ -15,15 +24,6 @@ export default function RootLayout({
 }) {
   return (
     <>
-      <head>
-        <meta name="description" content="Reveal your unfollowers."/>
-        <meta property="og:title" content="Hatecast - Reveal your unfollowers" />
-        <meta property="og:description" content="Reveal your unfollowers."/>
-        <meta property="og:url" content="https://hatecast.xyz/" />
-        <meta property="og:image" content="https://i.imgur.com/BRSiSUD.png" />
-        <meta property="og:type" content="website" />
-        <meta property="og:locale" content="en_US" />
-      </head>
       <html lang="en" className={inter.className}>
         <body>{children}</body>
         <Analytics />
